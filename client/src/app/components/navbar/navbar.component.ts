5dca5083d432e2c6f7f4e74b34ebf4235c11a49c
Yassine <yakkari@unionkeynet.com>
2019-08-05 09:43:56 +0100

    jwt login

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service.service";
+import { Router } from "@angular/router";
+import { FlashMessagesService } from "angular2-flash-messages";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public flashMessagesService: FlashMessagesService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  // Function to logout user
+  onLogoutClick() {
+    this.authService.logout(); // Logout user
+    this.router.navigate(["/"]); // Navigate back to home page
   }
 
+  ngOnInit() {}
 }
